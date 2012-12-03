534d3fec25d35958aba272414ae7592bef09416b
ikhlas ahmed <ikhlas_06@hotmail.com>
2012-12-03 10:14:36 -0500

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-welcome to index.php
+<div id='page_wrapper'>
+	<div id='image_wrapper'>
+        <div ='back_image_div'>
+            <div id='id_div'>
+            </div>
+        </div>
+    </div>
+    
+    <div id='quick_wrapper'>
+    	<div id='info_div'>
+        </div>
+        <div id='friend_div'>
+        </div>
+        <div id='albumn_video_div'>
+        </div>
+        <div id='blog'>
+        </div>
+    </div>
+    
+    <div id='detail_wrapper'>
+    	<div id='edu_div'>
+        </div>
+        <div id='work_div'>
+        </div>
+        <div id='fav_div'>
+        </div>
+    </div>
+    
+    
+    <div id='text_wrapper'>
+    <div>
+</div>
