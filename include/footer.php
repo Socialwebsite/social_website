e6969bb117c9fcdcf991a02befb434559b2f3b4a
ikhlas ahmed <ikhlas_06@hotmail.com>
2013-01-15 18:19:10 -0500

    del all my files

--- include/footer.php
+++
@@ -1,7 +0,0 @@
-	<!-- jquery script goes at eng of code --> <!-- path of script is src="js/jquery.js" -->
-		<script type = "text/javascript" src = "JQUERY/jquery_script.js"> </script> 
-		  <script type="text/javascript" src="jquery/coin-slider/coin-slider.min.js"></script>
-         <script type="text/javascript" src="jquery/coin-slider/coin-slider.js"></script>
-        <script type = "text/javascript" src = "JQUERY/jquery.js"> </script>  
-    </body>
-</html>
--- jquery/jquery.js
+++
@@ -1,4 +0,0 @@
-// JavaScript Document
-	$(document).ready(function() {
-		$('#coin-slider').coinslider();
-	});
